aee994befc9550da47a40ef5feb39e4f435c1417
Zandile Buthelezi <zbuthelezi023@student.wethinkcode.co.za>
2024-08-22 18:32:17 +0200

    signup and login are done

--- app.py
+++ app.py
@@ -6,25 +6,34 @@ app = Flask(__name__)
 
 @app.route('/')
 def landing_page():
-    return render_template('index.html')
+    return render_template('signup.html')
 
-# Function to save user login information
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form['username']
+# Function to handle user signup
+@app.route('/signup', methods=['POST'])
+def signup():
+    name = request.form['name']
+    surname = request.form['surname']
+    email = request.form['email']
     password = request.form['password']
     fnb_user = request.form.get('fnb_user')  # Capture checkbox value
 
-    save_user_info(username, password, fnb_user)
+    # Save the user info and then redirect to login page
+    save_user_info(name, surname, email, password, fnb_user)
 
-    return redirect(url_for('landing_page'))
+    return render_template('index.html')
 
-# Function to save user login information
-def save_user_info(username, password, fnb_user, file_path='user_data.json'):
+# Function to handle user login
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+def save_user_info(name, surname, email, password, fnb_user, file_path='user_data.json'):
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
     user_info = {
-        "username": username,
+        "name": name,
+        "surname": surname,
+        "email": email,
         "password": hashed_password.decode('utf-8'),
         "fnb_user": True if fnb_user else False  # Save checkbox state as boolean
     }
@@ -39,7 +48,6 @@ def save_user_info(username, password, fnb_user, file_path='user_data.json'):
 
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
