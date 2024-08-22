0d4eb9c62e58bb266e50ff69696c9c2b3a8488b5
wtc <wtc@pop-os.localdomain>
2024-08-22 16:38:02 +0200

    front end team on check

--- app.py
+++ app.py
@@ -1,38 +1,42 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import json
 import bcrypt
 
 app = Flask(__name__)
 
-# Function to save user login information
-def save_user_info(username, password, file_path='user_data.json'):
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+@app.route('/')
+def landing_page():
+    return render_template('index.html')
 
-    user_info = {
-        "username": username,
-        "password": hashed_password.decode('utf-8')
-    }
+# # Function to save user login information
+# def save_user_info(username, password, file_path='user_data.json'):
+#     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-    try:
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        data = []
+#     user_info = {
+#         "username": username,
+#         "password": hashed_password.decode('utf-8')
+#     }
 
-    data.append(user_info)
+#     try:
+#         with open(file_path, 'r') as file:
+#             data = json.load(file)
+#     except FileNotFoundError:
+#         data = []
 
-    with open(file_path, 'w') as file:
-        json.dump(data, file, indent=4)
+#     data.append(user_info)
+
+#     with open(file_path, 'w') as file:
+#         json.dump(data, file, indent=4)
 
 
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
+# @app.route('/login', methods=['POST'])
+# def login():
+#     username = request.form['username']
+#     password = request.form['password']
 
-    save_user_info(username, password)
+#     save_user_info(username, password)
 
-    return jsonify({"message": "User login information saved."})
+#     return jsonify({"message": "User login information saved."})
 
 if __name__ == '__main__':
     app.run(debug=True)
