3371df6fab34365265fce433131b73c5d70906ed
WTC <wtc@pop-os.localdomain>
2024-08-23 13:38:52 +0200

    latest change

---
+++ new/index.js
@@ -0,0 +1,52 @@
+let ourClients = []
+    
+class User {
+
+    constructor(name, contact, country, birthDay) {
+        this.name = name;
+        this.contact = contact;
+        this.country = country;
+        this.birthDay = birthDay;
+    }
+
+    toString() {
+    return `${this.contact}`;
+  }
+    
+    
+}
+
+
+function addUser() {
+    let name = document.getElementById('firstName').value;
+    let country = document.getElementById('birthCountry').value;
+    let bd = document.getElementById('dob').value;
+    let contact = String(document.getElementById('contactDetails').value);
+    let myUser = new User(name, contact, country, bd)
+    ourClients[ourClients.length] = myUser
+    console.log(userExists(contact))
+    
+}
+
+
+function userExists(contact) {
+
+    for(let j = 0; j < ourClients.length; j++) {
+        count = 0
+        for(let i = 0; i < contact.length; i++) {
+            if(ourClients[j].contact.length != contact.length) {continue}
+            if(ourClients[j].contact[i] == contact[i]) {count++}
+        }
+        if(count == contact.length) {
+            window.location.href = 'get_points.html'; // send user to different page when successfully logged in
+            return true
+        }
+    }
+    return false
+}
+
+
+
+
+
+
